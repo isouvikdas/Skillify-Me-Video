8cb7493f42d97e8104b9a013fe3be2f53311348b
isouvikdas <developersouvik02@gmail.com>
2024-08-06 14:02:02 +0530

    git initialized

---
+++ src/main/java/com/skillifyme/video/Skillify_Me_Video/SkillifyMeVideoApplication.java
@@ -0,0 +1,13 @@
+package com.skillifyme.video.Skillify_Me_Video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillifyMeVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillifyMeVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillifyme/video/Skillify_Me_Video/SkillifyMeVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillifyme.video.Skillify_Me_Video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillifyMeVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
